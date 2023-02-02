a03455d304a7f2fa21eba1913b58e5981a80b83c
Enzo Perrin <75301600+oui80@users.noreply.github.com>
2023-02-02 19:04:26 +0100

    Revert "OUI"
    This reverts commit d944ce72ce9a62a31fa69207ce6170be6b811a2e.

--- android/app/src/main/kotlin/fr/perrinindustries/nomaislaoh/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package fr.perrinindustries.nomaislaoh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/boxes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive/hive.dart';
-import 'model/Partie.dart';
-
-class Boxes {
-  static Box<Partie> getparties()=>
-      Hive.box<Partie>('Parties');
-}
